fd56b0a6c4741e108f31c50b8906d1e57c39f0cc
Parkchangjin <brainnia@naver.com>
2021-02-12 05:46:53 +0000

    For and while example

--- main.py
+++ main.py
@@ -5,3 +5,46 @@ print("connection testing 2")
 print("Hello github on iPad")
 print("now i can write my python code with iPad")
 print("정보처리산업기능사")
+
+def chat(name1, name2, age):
+  print("%s : hello? how old are you?" %name1)
+  print("%s : me? i'm %d" %(name2, age))
+  
+chat("alex", "mini", 10)
+chat("daniel", "chul", 20)
+
+def dsum(a,b):
+  result = a+b
+  return result
+  
+d = dsum(2,4)
+print(d)
+
+#for example
+for i in range(3):
+  print("say ho!")
+  
+#while example
+i = 0
+while i<3:
+  print("say hell yeah")
+  i = i+1
+  
+while True:
+  print("say anything")
+  print(i)
+  i = i+1
+  if i > 4:
+    break
+  
+for j in range(3):
+  print("oh no")
+  print("yes yes")
+  
+  if j ==1:
+    continue
+  
+  print("oh my god")
+  
+  
+
